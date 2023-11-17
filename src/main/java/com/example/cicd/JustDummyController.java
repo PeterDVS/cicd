ce6c5195ce5968ebf7005d681afc5b5fdd29b3a2
Peter Devos <peter.devos9#telenet.be>
2023-11-17 10:31:10 +0100

    iets

--- src/main/java/com/example/cicd/JustDummyController.java
+++ src/main/java/com/example/cicd/JustDummyController.java
@@ -1,14 +1,12 @@
 package com.example.cicd;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/cicd")
+@RestController
 public class JustDummyController {
-    @GetMapping
-    public String get() {
+    @GetMapping("/cicd")
+    public String getd() {
         return "Yep";
     }
 }
--- src/test/java/com/example/cicd/JustDummyControllerTest.java
+++ src/test/java/com/example/cicd/JustDummyControllerTest.java
@@ -1,19 +1,21 @@
 package com.example.cicd;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(JustDummyControllerTest.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class JustDummyControllerTest {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
 
@@ -21,7 +23,7 @@ class JustDummyControllerTest {
     void getd() throws Exception {
 
         mockMvc.perform(get("/cicd"))
-                .andExpect(status().isOk())
-                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString("Yep")));
+                .andExpect(status().isOk());
+
     }
 }
