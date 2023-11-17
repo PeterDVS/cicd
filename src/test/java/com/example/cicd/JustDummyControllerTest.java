804ebf31b7b452f78a48d319d7ae219191d5b766
Peter Devos <peter.devos9#telenet.be>
2023-11-17 09:38:30 +0100

    iets

--- src/test/java/com/example/cicd/JustDummyControllerTest.java
+++ src/test/java/com/example/cicd/JustDummyControllerTest.java
@@ -1,13 +1,11 @@
 package com.example.cicd;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
